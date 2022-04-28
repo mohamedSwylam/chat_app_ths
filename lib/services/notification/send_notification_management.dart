1efe6c4b7c9b75a3495e4d794d5c453f0afc716f
User Namegit config --global user.name User <mohamedswylam9@gmail.com>
2022-04-28 23:50:20 +0200

    chatApp

--- lib/services/notification/send_notification_management.dart
+++ lib/services/notification/send_notification_management.dart
@@ -49,44 +49,4 @@ class SendNotification {
     }
   }
 
-  Future<int> sendNotification(
-      {required String token,
-      required String title,
-      required String body}) async {
-    try {
-      print("In Notification");
-
-      final String _serverKey =
-          "AAAAEIOZbL4:APA91bFhZGG9Jv5dmHUiDL6fzbPBJPwKTSKYICqYzAwIlt36eCGGYeJZRwryJo9MT-8V6AnrcUhk1Sr3WYMP3uM392LybID4KAIoox3DdonsxuBwS9zYIfHqZUQl3qahxjDsU3jfL8xm";
-
-      final Response response = await post(
-        Uri.parse("https://fcm.googleapis.com/fcm/send"),
-        headers: <String, String>{
-          "Content-Type": "application/json",
-          "Authorization": "key=$_serverKey",
-        },
-        body: jsonEncode(<String, dynamic>{
-          "notification": <String, dynamic>{
-            "body": body,
-            "title": title,
-          },
-          "priority": "high",
-          "data": <String, dynamic>{
-            "click": "FLUTTER_NOTIFICATION_CLICK",
-            "id": "1",
-            "status": "done",
-            "collapse_key": "type_a",
-          },
-          "to": token,
-        }),
-      );
-
-      print("Response is: ${response.statusCode}   ${response.body}");
-
-      return response.statusCode;
-    } catch (e) {
-      print("Error in Notification Send: ${e.toString()}");
-      return 404;
-    }
-  }
 }
